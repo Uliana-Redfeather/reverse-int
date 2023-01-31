55aa17a334314f3e23aee4a94cc20c52084f222e
Uliana-Redfeather <111862073+Uliana-Redfeather@users.noreply.github.com>
2023-01-31 18:03:14 +0700

    init: reverse-int done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  if (n<0){
+    n=n*(-1);
+  }
+let str = '';
+let resultstr = '';
+str = n.toString();
+for (i=0;i<str.length;i++){
+resultstr = str[i] + resultstr;
+}
+return resultstr *1;
+
+
 }
